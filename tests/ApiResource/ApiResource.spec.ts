cdc5b1120f3e6a8bca6f5e4f113db6e782af524d
Michal Palys-Dudek <michal@palys-dudek.pl>
2021-03-16 14:51:30 +0100

    ✅ Streamline tests for @ApiResource.

--- tests/ApiResource/ApiResource.spec.ts
+++ tests/ApiResource/ApiResource.spec.ts
@@ -5,75 +5,99 @@ import { Recipe } from '../fixtureapp/entities/Recipe';
 
 describe('@ApiResource', (): void => {
   const tt = new TestingTool(app);
+  let recipe: Recipe;
+  let ingredient: Ingredient;
 
-  describe('Generates LCRUD endpoints for entity decorated with @ApiResource (Recipe)', (): void => {
-    test.todo('POST /recipes creates an entity');
-
-    test.todo('GET /recipes lists entities');
-
-    test('GET /recipes/:id returns the entity', async (): Promise<void> => {
-      const repository = tt.getRepository(Recipe);
-      const recipe = repository.create({
+  beforeAll(
+    async (): Promise<void> => {
+      recipe = await tt.createEntity(Recipe, {
         title: 'Tiramisu',
         description: 'Most delicious cake.',
         complexity: 1,
         timeRequired: 15,
       });
-      await repository.save(recipe);
 
-      const { body } = await tt.get(`/recipes/${recipe.id}`).expect(200);
-      expect(body).toStrictEqual({
-        data: {
-          id: recipe.id,
-          createdAt: expect.any(String),
-          updatedAt: expect.any(String),
-          title: 'Tiramisu',
-          description: 'Most delicious cake.',
-          complexity: 1,
-          timeRequired: 15,
-        },
-        meta: {},
+      ingredient = await tt.createEntity(Ingredient, {
+        name: 'Mascarpone',
+      });
+    },
+  );
+
+  describe('Generates LCRUD endpoints for entity decorated with @ApiResource', (): void => {
+    describe('POST /resources', (): void => {
+      test.todo('validates the input');
+
+      test.todo('creates the entity and returns it in the response');
+
+      test.todo(
+        'returns 404 for resources that have not enabled this operation',
+      );
+    });
+
+    describe('GET /resources', (): void => {
+      test.todo('lists entities');
+
+      test.todo('returns pagination meta data');
+
+      test.todo('listings allow to go to next pages');
+
+      test.todo(
+        'returns 404 for resources that have not enabled this operation',
+      );
+    });
+
+    describe('GET /resources/:id', (): void => {
+      test('returns 404 if entity not found', async (): Promise<void> => {
+        await tt.get('/recipes/123124').expect(404);
+      });
+
+      test.todo('returns 404 if id param is too long');
+
+      test('returns the entity', async (): Promise<void> => {
+        const { body } = await tt.get(`/recipes/${recipe.id}`).expect(200);
+        expect(body).toStrictEqual({
+          data: {
+            id: recipe.id,
+            createdAt: expect.any(String),
+            updatedAt: expect.any(String),
+            title: 'Tiramisu',
+            description: 'Most delicious cake.',
+            complexity: 1,
+            timeRequired: 15,
+          },
+          meta: {},
+        });
+      });
+
+      test.todo(
+        'returns 404 for resources that have not enabled this operation',
+      );
+    });
+
+    describe('PATCH /resources/:id', (): void => {
+      test.todo('returns 404 if id param is too long');
+
+      test.todo('returns 404 is entity is not found');
+
+      test.todo('validates the input');
+
+      test.todo('updates the entity and returns updated data');
+
+      test('returns 404 for resources that have not enabled this operation', async (): Promise<void> => {
+        await tt.patch(`/recipe-ingredients/${ingredient.id}`, {}).expect(404);
       });
     });
 
-    test.todo('PATCH /recipes/:id updates the entity and returns updated data');
+    describe('DELETE /resources/:id', (): void => {
+      test.todo('returns 404 if id param is too long');
 
-    test.todo('DELETE /recipes/:id deletes the entity');
-  });
+      test.todo('returns 404 if entity is not found');
 
-  describe('Generates LCRUD endpoints for enabled operations with @ApiResource({ operations: []}) param (RecipeIngredient)', (): void => {
-    let ingredient: Ingredient;
+      test.todo('deletes the entity');
 
-    beforeAll(
-      async (): Promise<void> => {
-        const repository = tt.getRepository(Ingredient);
-        ingredient = repository.create({
-          name: 'Mascarpone',
-        });
-        await repository.save(ingredient);
-      },
-    );
-
-    test.todo('GET /recipe-ingredients lists entities');
-
-    test.todo('POST /recipe-ingredients creates an entities');
-
-    test.todo('GET /recipe-ingredients/:id returns the entity');
-
-    test('PATCH /recipe-ingredients/:id returns 404', async (): Promise<void> => {
-      await tt.patch(`/recipe-ingredients/${ingredient.id}`, {}).expect(404);
+      test('returns 404 for resources that have not enabled this operation', async (): Promise<void> => {
+        await tt.delete(`/recipe-ingredients/${ingredient.id}`).expect(404);
+      });
     });
-
-    test('DELETE /recipe-ingredients/:id returns 404', async (): Promise<void> => {
-      await tt.delete(`/recipe-ingredients/${ingredient.id}`).expect(404);
-    });
-  });
-
-  describe('Listing endpoint can be paginated', (): void => {
-    test.todo('Listings return pagination meta data');
-
-    test.todo('Pagination meta data includes next and previous links');
-
-    test.todo('Listings allow to go to next pages');
   });
 });
